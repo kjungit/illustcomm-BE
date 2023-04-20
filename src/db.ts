b5512af38ce7592cff3a47cbc834197dc5d1ba97
kjun <kjun_all@naver.com>
2023-04-20 13:35:37 +0900

    :package: Edit db.ts

--- src/db.ts
+++ src/db.ts
@@ -9,7 +9,11 @@ export const myDataBase = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: "illustcommdb", // db 이름
-  entities: ["src/entity/*.ts"], // 모델의 경로
+  entities: [
+    process.env.NODE_ENV === "production"
+      ? "dist//entity/*.js"
+      : "src/entity/*.ts",
+  ], // 모델의 경로
   logging: true, // 정확히 어떤 sql 쿼리가 실행됐는지 로그 출력
   synchronize: true, // 현재 entity 와 실제 데이터베이스 상 모델을 동기화
 });
